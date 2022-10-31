20475230db2c17a0ce9adc0a002c21c9ffe40fb3
yuxxeun <itsyuxxeun@gmail.com>
2022-10-31 15:43:31 +0700

    Add twitter SEO

---
+++ components/Meta.jsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+
+export default function Meta() {
+  return (
+    <Head>
+      <title>ᴠᴀʟɪɴᴀᴛɪᴄ</title>
+      <meta name="title" content="ᴠᴀʟɪɴᴀᴛɪᴄ" />
+      <meta name="robots" content="noindex, nofollow" />
+      <meta name="description" content="THE HOME OF REBEL DEVELOPER'S." />
+      <meta name="robots" content="noindex, nofollow" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <meta name="revisit-after" content="7 days" />
+      <meta name="author" content="@valinatic" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://valinatic.tech/" />
+      <meta property="og:title" content="ᴠᴀʟɪɴᴀᴛɪᴄ" />
+      <meta
+        property="og:description"
+        content="THE HOME OF REBEL DEVELOPER'S."
+      />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/valinatic/.github/main/assets/wicked.jpg"
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://valinatic.tech/" />
+      <meta property="twitter:title" content="ᴠᴀʟɪɴᴀᴛɪᴄ" />
+      <meta
+        property="twitter:description"
+        content="THE HOME OF REBEL DEVELOPER'S."
+      />
+      <meta
+        property="twitter:image"
+        content="https://raw.githubusercontent.com/valinatic/.github/main/assets/wicked.jpg"
+      />
+    </Head>
+  );
+}
